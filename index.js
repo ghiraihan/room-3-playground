45cf8e1ac92370165de699b5b19dcf34f99a0085
Antonius Bonni Febrianto <antoniusbonni@gmail.com>
2024-03-16 22:21:26 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+// 1. panggil dulu module expressnya
+const express = require ('express');
+const app = express();
+
+app.use(express.urlencoded());
+// // 3. Running servernya
+const PORT = 6666;
+
+app.listen(PORT, function () {
+    console.log(`Server berjalan di localhost:${PORT}`);
+});
