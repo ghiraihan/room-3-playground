604143e4c88447df2bbd0bcdb7dc37e1bd2bc834
ilhamnahadi <ilhamnahadi@gmail.com>
2024-03-16 23:49:05 +0700

    revisi index.js

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.use(express.urlencoded());
 // // 3. Running servernya
 const PORT = 6666;
 
+// todo : Controller
+const lihat_history = require("./controller/lihat-history");
+const hitungLuas = require("./controller/hitung-luas");
+const hitungKeliling =require("./controller/hitung-keliling");
+
+// todo : Routing
+server.get("/lihat-history", lihat_history);
+
+server.post('/hitung-luas', hitungLuas)
+
+server.post('/hitung-keliling', hitungKeliling)
+
 app.listen(PORT, function () {
     console.log(`Server berjalan di localhost:${PORT}`);
 });
