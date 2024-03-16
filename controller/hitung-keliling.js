8858753a2cb2ce767197ca595e1d9e0770e11aae
ilhamnahadi <ilhamnahadi@gmail.com>
2024-03-16 23:41:57 +0700

    keliling 2-ilham

--- controller/hitung-keliling.js
+++ controller/hitung-keliling.js
@@ -4,11 +4,11 @@ function kelilingBelahKetupat (req, res) {
     // 1. ambil data dari client
     const data = req.body;
     const sisi = data.sisi
- 
+
     // 2. hitung keliling
     const keliling = 4*sisi;
     res.status(200).send("Keliling:" + keliling);
-
+    
   history.push({
     sisi: sisi,
     Keliling: keliling,
