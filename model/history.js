1fcbab6434391c8b6ae2a952ec8daf4b430633cb
sabilaqy <sabila.sqy@gmail.com>
2024-04-04 22:38:20 +0700

    update history

--- model/history.js
+++ model/history.js
@@ -2,8 +2,7 @@ const client = require('./dbConnection')
 async function history(req,res){
     //ambil data dari db
     const data = await client.query(`
-    SELECT * FROM history;
-    `);
+    SELECT * FROM history;`);
     res.status(200).json({data: [...data.rows]});
 }
 module.exports = history;
