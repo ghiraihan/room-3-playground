c7b1643eee1ba3a345f29e52e80795b344138a86
panpai04 <irpan.pauzi16@gmail.com>
2024-04-04 22:41:26 +0700

    history

--- model/history.js
+++ model/history.js
@@ -1,3 +1,16 @@
-const history = [];
+// const history = [];
 
+// module.exports = history;
+
+const client = require('./dbConnection')
+async function history(req,res){
+    //ambil data dari db
+    const data = await client.query(`
+    SELECT * FROM history;
+    `);
+    // console.log(data.rows);
+    // console.log(data);
+    // kasih response
+    res.status(200).json({data: [...data.rows]});
+}
 module.exports = history;
